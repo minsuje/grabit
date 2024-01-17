0ef6f8c688b72e2e3a2f9bf4bdabba9de3ca00b8
seongRyoung <tjdfyd6619@gmail.com>
2024-01-17 16:10:51 +0900

    create Folder create(challenge, home, myPage)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,47 +1,49 @@
-import './App.css'
-import { Button } from './components/ui/button'
-import { Input } from './components/ui/input'
-import { Label } from "@/components/ui/label"
-import { Tab, CreateChallenge, Record, HotChallenge } from '@/components/Component0117'
+import './App.css';
+import { Button } from './components/ui/button';
+import { Input } from './components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Tab, CreateChallenge, Record, HotChallenge } from '@/components/Component0117';
+import { ListComponent1, SelectComponent2 } from './components/ComponentSeong';
 
 function App() {
+    return (
+        <div className="App">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-6">
+                <div className="list flex flex-col w-full gap-2">
+                    <h3 className="flex text-md text-gray-400 font-bold hover:text-red-500 transition-all hover:text-lg">
+                        라벨
+                    </h3>
+                    <input type="text" className="flex px-2 py-2 border-2 border-gray-400 rounded-lg" />
+                </div>
+                <div className="list flex flex-col w-full gap-2">
+                    <h3 className="flex text-md text-gray-400 font-bold">라벨</h3>
+                    <input type="text" className="flex px-2 py-2 border-2 border-gray-400 rounded-lg" />
+                </div>
+                <div className="list flex flex-col w-full gap-2">
+                    <h3 className="flex text-md text-gray-400 font-bold">라벨</h3>
+                    <input type="text" className="flex px-2 py-2 border-2 border-gray-400 rounded-lg" />
+                </div>
+            </div>
 
-  return (
-    <div className="App">
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-6">
-        <div className='list flex flex-col w-full gap-2'>
-          <h3 className='flex text-md text-gray-400 font-bold hover:text-red-500 transition-all hover:text-lg'>라벨</h3>
-          <input type="text" className='flex px-2 py-2 border-2 border-gray-400 rounded-lg' />
+            <Button>하이하이</Button>
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="email">Email</Label>
+                <Input type="email" id="email" placeholder="Email" />
+            </div>
+
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="picture">Picture</Label>
+                <Input id="picture" type="file" />
+            </div>
+
+            <Tab />
+            <CreateChallenge />
+            <HotChallenge />
+            <Record />
+            <ListComponent1 />
+            <SelectComponent2 />
         </div>
-        <div className='list flex flex-col w-full gap-2'>
-          <h3 className='flex text-md text-gray-400 font-bold'>라벨</h3>
-          <input type="text" className='flex px-2 py-2 border-2 border-gray-400 rounded-lg' />
-        </div>
-        <div className='list flex flex-col w-full gap-2'>
-          <h3 className='flex text-md text-gray-400 font-bold'>라벨</h3>
-          <input type="text" className='flex px-2 py-2 border-2 border-gray-400 rounded-lg' />
-        </div>
-      </div>
-
-      <Button>하이하이</Button>
-      <div className="grid w-full max-w-sm items-center gap-1.5">
-        <Label htmlFor="email">Email</Label>
-        <Input type="email" id="email" placeholder="Email" />
-      </div>
-
-
-      <div className="grid w-full max-w-sm items-center gap-1.5">
-        <Label htmlFor="picture">Picture</Label>
-        <Input id="picture" type="file" />
-      </div>
-
-      <Tab />
-      <CreateChallenge />
-      <HotChallenge />
-      <Record />
-
-    </div>
-  )
+    );
 }
 
-export default App
+export default App;
