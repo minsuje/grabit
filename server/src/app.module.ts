f2c58fd2506b60acbab649b1210519234583c149
yewonkim301 <yeowne301@gmail.com>
2024-01-18 15:16:50 +0900

    feat: challenge module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChallengeModule } from './modules/challenge/challenge.module';
 
 @Module({
-  imports: [],
+  imports: [ChallengeModule],
   controllers: [AppController],
   providers: [AppService],
 })
