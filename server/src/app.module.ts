46c1a0130d0ba33039aacd91cb13a2eec2e83e33
minsuje <minsu.jex@gmail.com>
2024-01-18 17:58:04 +0900

    fix: add app

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,7 +6,7 @@ import { AccountModule } from './modules/account/account.module';
 import { DailyMissionModule } from './modules/daily_mission/daily_mission.module';
 import { FollowModule } from './modules/follow/follow.module';
 import { ChallengeModule } from './modules/challenge/challenge.module';
-import { AlarmModule } from './alarm/alarm.module';
+import { AlarmModule } from './modules/alarm/alarm.module';
 
 @Module({
     imports: [UserModule, AccountModule, FollowModule, DailyMissionModule, ChallengeModule, AlarmModule],
