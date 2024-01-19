48337339985d777b111c1050c25159d38ecb91ab
seejnn <seejinkim@gmail.com>
2024-01-19 18:58:21 +0900

    feat: setup github actions

--- server/src/modules/user/user.controller.ts
+++ server/src/modules/user/user.controller.ts
@@ -5,10 +5,13 @@ import { db } from '../../../db/db';
 import { users } from './schema';
 @Controller('user')
 export class UserController {
-    constructor(private userService: UserService) {}
+  constructor(private userService: UserService) {}
 
-    @Post('/register/:type')
-    createUserDto(@Param('type') type: string, @Body() createUserDto: CreateUserDto): any {
-        return db.insert(users).values(createUserDto);
-    }
-
+  @Post('/register/:type')
+  createUserDto(
+    @Param('type') type: string,
+    @Body() createUserDto: CreateUserDto,
+  ): any {
+    return db.insert(users).values(createUserDto);
+  }
+}
