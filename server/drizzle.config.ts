e90bdf106686ff12703229bb40a1ccbc2caa3d82
seejnn <seejinkim@gmail.com>
2024-01-19 11:06:55 +0900

    feat: add tier, score

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -3,10 +3,16 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    schema: ['./src/modules/user/schema.ts', './src/modules/account/schema.ts', './src/modules/challenge/schema.ts', './src/modules/alarm/schema.ts','./src/modules/daily_mission/schema.ts' ],
-    out: './migration',
-    driver: 'pg',
-    dbCredentials: {
-        connectionString: process.env.DATABASE_URL,
-    },
+  schema: [
+    './src/modules/user/schema.ts',
+    './src/modules/account/schema.ts',
+    './src/modules/challenge/schema.ts',
+    './src/modules/alarm/schema.ts',
+    './src/modules/daily_mission/schema.ts',
+  ],
+  out: './migration',
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL,
+  },
 } satisfies Config;
