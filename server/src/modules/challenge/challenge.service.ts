419e69a0b4318a480b76340b47dd11168a731719
yewonkim301 <yeowne301@gmail.com>
2024-01-19 11:19:47 +0900

    feat: challenge controller

--- server/src/modules/challenge/challenge.service.ts
+++ server/src/modules/challenge/challenge.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { ChallengeCreateDto } from './dto/challenge-create.dto';
 
 @Injectable()
-export class ChallengeService {}
+export class ChallengeCreateService {
+  createNewChallenge(challengeCreateDto: ChallengeCreateDto) {
+    const {
+      userid_num,
+      challenge_name,
+      topic,
+      challenger_userid_num,
+      goal_money,
+      deadline,
+      authentication_term,
+      authentication_time,
+    } = challengeCreateDto;
+    const challenge = {
+      userid_num,
+      challenge_name,
+      topic,
+      challenger_userid_num,
+      goal_money,
+      deadline,
+      authentication_term,
+      authentication_time,
+    };
+  }
+}
