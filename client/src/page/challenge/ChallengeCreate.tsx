e3e85bc509d6f9f04ba4f0102ff18683a04f683d
seejnn <seejinkim@gmail.com>
2024-01-20 19:47:05 +0900

    feat: createChallenge

--- client/src/page/challenge/ChallengeCreate.tsx
+++ client/src/page/challenge/ChallengeCreate.tsx
@@ -1,199 +1,208 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import { Tab } from '@/components/Component0117'
-import { Switch } from '@/components/ui/switch'
-import { Label } from '@/components/ui/label'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Tab } from '@/components/Component0117';
+import { Switch } from '@/components/ui/switch';
+import { Label } from '@/components/ui/label';
 import { ListComponent1 } from '../../components/ComponentSeong';
-import {
-  Avatar, AvatarFallback,
-  AvatarImage,
-} from '@/components/ui/avatar';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Calendar } from '@/components/ui/calendar';
 
+import * as React from 'react';
+import { useState } from 'react';
+import { addDays, format } from 'date-fns';
+import { ko } from 'date-fns/locale';
+import { Calendar as CalendarIcon } from 'lucide-react';
+import { DateRange } from 'react-day-picker';
 
-import * as React from "react"
-import { addDays, format } from "date-fns"
-import { kr, ko } from 'date-fns/locale';
-import { Calendar as CalendarIcon } from "lucide-react"
-import { DateRange } from "react-day-picker"
+import { cn } from '@/lib/utils';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import axios from 'axios';
 
-import { cn } from "@/lib/utils"
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
+function ChallengeCreate({ className }: React.HTMLAttributes<HTMLDivElement>) {
+    const [date, setDate] = useState<DateRange | undefined>({
+        from: new Date(2022, 0, 20),
+        to: addDays(new Date(2022, 0, 20), 20),
+    });
+    const [isPublic, setIsPublic] = useState<boolean>(false);
+    const [topic, setTopic] = useState<string>('');
+    const [authTerm, setAuthTerm] = useState<string>('');
+    const [authTime, setAuthTime] = useState<string>('');
 
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
+    const tab1content = (
+        <div>
+            <div className="user-list flex">
+                <h2 className="flex w-full text-xl font-bold py-4">참여자</h2>
+                <div className="flex w-fit items-center space-x-2">
+                    <Switch id="public" checked={isPublic} onCheckedChange={setIsPublic} />
+                    <Label htmlFor="public" className="w-8">
+                        공개
+                    </Label>
+                </div>
+            </div>
 
-
-
-
-function ChallengeCreate({
-  className,
-}: React.HTMLAttributes<HTMLDivElement>) {
-
-  const [date, setDate] = React.useState<DateRange | undefined>({
-    from: new Date(2022, 0, 20),
-    to: addDays(new Date(2022, 0, 20), 20),
-  })
-
-
-  const tab1content = (
-    <div>
-
-      <div className="user-list flex">
-        <h2 className='flex w-full text-xl font-bold py-4'>참여자</h2>
-        <div className="flex w-fit items-center space-x-2">
-          <Switch id="public" />
-          <Label htmlFor="public" className='w-8'>공개</Label>
+            <div className="user-list flex flex-col gap-4">
+                <div className="flex items-center gap-2">
+                    <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <span>홍길동</span>
+                </div>
+                <Button>추가하기</Button>
+            </div>
         </div>
-      </div>
+    );
 
-      <div className="user-list flex flex-col gap-4">
-        <div className='flex items-center gap-2'>
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <span>홍길동</span>
+    const tab2content = (
+        <div>
+            <div className="user-list flex">
+                <h2 className="flex w-full text-xl font-bold py-4">참여자</h2>
+                <div className="flex w-fit items-center space-x-2">
+                    <Switch id="public" checked={isPublic} onCheckedChange={setIsPublic} />
+                    <Label htmlFor="public" className="w-8">
+                        공개
+                    </Label>
+                </div>
+            </div>
+
+            <div className="user-list flex flex-col gap-4">
+                <div className="flex items-center gap-2">
+                    <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <span>홍길동</span>
+                </div>
+                <div className="flex items-center gap-2">
+                    <Avatar>
+                        <AvatarImage src="https://github.com/kwonkuwhi.png" alt="@shadcn" />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <span>홍길동</span>
+                </div>
+                <div className="flex items-center gap-2">
+                    <Avatar>
+                        <AvatarImage src="https://github.com/seejnn.png" alt="@shadcn" />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <span>홍길동</span>
+                </div>
+                <Button>추가하기</Button>
+            </div>
         </div>
-        <Button>추가하기</Button>
-      </div>
+    );
 
+    async function handleSubmit() {
+        console.log(date);
+        console.log(topic);
+        console.log(isPublic);
+        console.log(authTerm);
+        console.log(authTime);
 
-    </div>
-  )
+        const result = await axios({
+            method: 'POST',
+            url: 'http://43.201.22.60:3000/challengeCreate',
+            data: {
+                challenge_name: '물 안마시기',
+                is_public: isPublic,
+                topic,
+                challenger_userid_num: [1, 2],
+                goal_money: 10000,
+                deadline: '2022-01-20',
+                authentication_start_date: '2022-01-20',
+                authentication_end_date: '2022-01-23',
+                authentication_start_time: 23,
+                authentication_end_time: 457,
+            },
+        });
+        console.log(result);
+    }
 
-  const tab2content = (
-    <div>
+    function handlePublic() {
+        console.log('isPublic');
+    }
 
-      <div className="user-list flex">
-        <h2 className='flex w-full text-xl font-bold py-4'>참여자</h2>
-        <div className="flex w-fit items-center space-x-2">
-          <Switch id="public" />
-          <Label htmlFor="public" className='w-8'>공개</Label>
-        </div>
-      </div>
+    return (
+        <div className="container ">
+            <h1 className="text-3xl font-bold py-4">챌린지 생성</h1>
 
-      <div className="user-list flex flex-col gap-4">
-        <div className='flex items-center gap-2'>
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <span>홍길동</span>
-        </div>
-        <div className='flex items-center gap-2'>
-          <Avatar>
-            <AvatarImage src="https://github.com/kwonkuwhi.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <span>홍길동</span>
-        </div>
-        <div className='flex items-center gap-2'>
-          <Avatar>
-            <AvatarImage src="https://github.com/seejnn.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <span>홍길동</span>
-        </div>
-        <Button>추가하기</Button>
-      </div>
+            <Tab tab1="1:1" tab2="그룹" tab1content={tab1content} tab2content={tab2content} />
 
+            <h2 className="text-xl font-bold py-4">주제</h2>
+            <Input onChange={(e) => setTopic(e.target.value)} />
 
-    </div>
-  )
+            <h2 className="text-xl font-bold py-4">기간</h2>
+            <div className={cn('grid gap-2', className)}>
+                <Popover>
+                    <PopoverTrigger asChild>
+                        <Button
+                            id="date"
+                            variant={'outline'}
+                            className={cn(
+                                'w-full justify-start text-left font-normal',
+                                !date && 'text-muted-foreground',
+                            )}
+                        >
+                            <CalendarIcon className="mr-2 h-4 w-4" />
+                            {date?.from ? (
+                                date.to ? (
+                                    <>
+                                        {format(date.from, 'PPP EEE', { locale: ko })} -{' '}
+                                        {format(date.to, 'PPP EEE', { locale: ko })}
+                                    </>
+                                ) : (
+                                    format(date.from, 'PPP EEE', { locale: ko })
+                                )
+                            ) : (
+                                <span>날짜를 선택하세요</span>
+                            )}
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                            initialFocus
+                            mode="range"
+                            defaultMonth={date?.from}
+                            selected={date}
+                            onSelect={setDate}
+                            numberOfMonths={2}
+                            locale={ko}
+                        />
+                    </PopoverContent>
+                </Popover>
+            </div>
 
+            <h2 className="text-xl font-bold py-4">인증 주기</h2>
+            <Select onValueChange={(value) => setAuthTerm(value)}>
+                <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="인증 주기" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectItem value="3">주 3회</SelectItem>
+                    <SelectItem value="7">주 5회</SelectItem>
+                    <SelectItem value="every">매일</SelectItem>
+                </SelectContent>
+            </Select>
 
-  return (
-    <div className='container '>
-      <h1 className='text-3xl font-bold py-4'>챌린지 생성</h1>
-
-      <Tab tab1='1:1' tab2='그룹' tab1content={tab1content} tab2content={tab2content} />
-
-
-
-      <h2 className='text-xl font-bold py-4'>주제</h2>
-      <Input />
-
-      <h2 className='text-xl font-bold py-4'>기간</h2>
-      <div className={cn("grid gap-2", className)}>
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button
-              id="date"
-              variant={"outline"}
-              className={cn(
-                "w-[300px] justify-start text-left font-normal",
-                !date && "text-muted-foreground"
-              )}
-            >
-              <CalendarIcon className="mr-2 h-4 w-4" />
-              {date?.from ? (
-                date.to ? (
-                  <>
-                    {format(date.from, "LLL dd, y", { locale: ko })} -{" "}
-                    {format(date.to, "LLL dd, y", { locale: ko })}
-                  </>
-                ) : (
-                  format(date.from, "LLL dd, y", { locale: ko })
-                )
-              ) : (
-                <span>날짜를 선택하세요</span>
-              )}
+            <h2 className="text-xl font-bold py-4">인증 시간</h2>
+            <Select onValueChange={(value) => setAuthTime(value)}>
+                <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="인증 시간" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectItem value="3">오전 7시</SelectItem>
+                    <SelectItem value="7">주 5회</SelectItem>
+                    <SelectItem value="every">매일</SelectItem>
+                </SelectContent>
+            </Select>
+            <Button onClick={handleSubmit} className="w-full mt-12 mb-8">
+                생성하기
             </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-auto p-0" align="start">
-            <Calendar
-              initialFocus
-              mode="range"
-              defaultMonth={date?.from}
-              selected={date}
-              onSelect={setDate}
-              numberOfMonths={2}
-              locale={ko}
-            />
-          </PopoverContent>
-        </Popover>
-      </div>
-
-
-      <h2 className='text-xl font-bold py-4'>인증 주기</h2>
-      <Select>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="인증 주기" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="3">주 3회</SelectItem>
-          <SelectItem value="7">주 5회</SelectItem>
-          <SelectItem value="every">매일</SelectItem>
-        </SelectContent>
-      </Select>
-
-      <h2 className='text-xl font-bold py-4'>인증 시간</h2>
-      <Select>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="인증 시간" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="3">오전 7시</SelectItem>
-          <SelectItem value="7">주 5회</SelectItem>
-          <SelectItem value="every">매일</SelectItem>
-        </SelectContent>
-      </Select>
-
-
-    </div>
-  )
+        </div>
+    );
 }
 
-export default ChallengeCreate
+export default ChallengeCreate;
