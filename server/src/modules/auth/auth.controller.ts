a4bc8d47124cb6d998d8005498b3b79de4bc83ad
minsuje <minsu.jex@gmail.com>
2024-01-22 09:28:23 +0900

    fix: auth

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, Param, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/create-auth-dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('/')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('/login')
-    LoginDto(@Body() loginDto: LoginDto): any {
-        return this.authService.loginUser(loginDto);
+    @UseGuards(AuthGuard('local'))
+    LoginDto(@Request() req: Request, @Body() loginDto: LoginDto): any {
+        const loginUsers = this.authService.loginUser(loginDto);
+
+        if (!loginUsers) throw new HttpException('Invalid Credentials', 401);
+        return loginUsers;
     }
 }
--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/localstrategy';
 
 @Module({
     imports: [
+        PassportModule,
         JwtModule.register({
             secret: 'ewreaereh24hmaeTtseb',
             signOptions: { expiresIn: '24h' },
         }),
     ],
     controllers: [AuthController],
-    providers: [AuthService],
+    providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
