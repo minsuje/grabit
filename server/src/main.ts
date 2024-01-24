66bbd91007d53d3806300728eb9a6e1617237b21
minsuje <minsu.jex@gmail.com>
2024-01-24 11:11:51 +0900

    feat: made a cookie

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import { db } from '../db/db';
 import { users } from '../src/modules/user/schema';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
+    app.use(cookieParser());
 
     // getUsers();
 
