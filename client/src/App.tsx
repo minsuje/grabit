622130a2484a57cf8d079aa2314d725650d033de
seongRyoung <tjdfyd6619@gmail.com>
2024-01-26 12:07:21 +0900

    create page mypage: charge/edit/history/historyDetail

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,24 +15,28 @@ import ChallengeEdit from './page/challenge/ChallengeEdit';
 import ChallengeDetail from './page/challenge/ChallengeDetail';
 import ChallengeDaily from './page/challenge/ChallengeDaily';
 
-import MyPageEdit from './page/myPage/MyPageEdit';
-import MyPage from './page/myPage/MyPage';
 import '@/App.css';
 import Alarm from './page/home/Alarm';
 import ChallengeAccept from './page/challenge/ChallengeAccept';
 
+// MyPage
+import MyPage from './page/myPage/MyPage';
+import MyPageEdit from './page/myPage/MyPageEdit';
 import FollowList from './page/myPage/FollowList';
+import MypageWithdraw from './page/myPage/MyPageWithdraw';
+import MypageCharge from './page/myPage/MyPageCharge';
+import MyPageHistory from './page/myPage/MyPageHistory';
+import MyPageHistoryDetail from './page/myPage/MyPageHistoryDetail';
 
 import FileUploadTest from './page/FileUploadTest';
 import FriendSelect from './page/myPage/FriendSelect';
 import Test from './page/challenge/Test';
 
-import Layout from './page/Layout';
-import MypageWithdraw from './page/myPage/MypageWithdraw';
+// import Layout from './page/Layout';
+
 import Camera from './page/challenge/Camera';
 
 function App() {
-
   return (
     <div className="App">
       <BrowserRouter>
@@ -40,38 +44,43 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register/normal" element={<Register />} />
-          <Route path="/" element={<Layout />}>
-            <Route path="/main" element={<Main />} />
-            <Route path="/main2" element={<Main2 />} />
-            <Route path="/challengeInProgress/:challenge_id" element={<ChallengeInProgress />} />
-            <Route path="/camera/:challenge_id" element={<Camera />} />
+          {/* <Route path="/" element={<Layout />}> */}
+          <Route path="/main" element={<Main />} />
+          <Route path="/main2" element={<Main2 />} />
+          <Route path="/challengeInProgress/:challenge_id" element={<ChallengeInProgress />} />
+          <Route path="/camera/:challenge_id" element={<Camera />} />
 
-            {/* <Route path="/challengetear" element={<ChallengeTear />} /> */}
-            <Route path="/challengeresult" element={<ChallengeResult />} />
-            <Route path="/challengeImage/:authentication_id" element={<ChallengeImage />} />
-            <Route path="/challengeList" element={<ChallengeList />} />
-            <Route path="/challengeEdit/:challenge_id" element={<ChallengeEdit />} />
-            <Route path="/challengeDetail/:challenge_id" element={<ChallengeDetail />} />
-            <Route path="/challengeDaily/:mission_id" element={<ChallengeDaily />} />
-            <Route path="/alarm" element={<Alarm />} />
-            <Route path="/challengeAccept/:challenge_id" element={<ChallengeAccept />} />
-            <Route path="/fileupload" element={<FileUploadTest />} />
-            <Route path="/friendSelect" element={<FriendSelect />} />
-            <Route path="/challengeResult/:challenge_id" element={<ChallengeResult />} />
-            <Route path="/challengeNotice" element={<ChallengeNotice />} />
-            <Route path="/challengeCreate" element={<ChallengeCreate />} />
+          {/* <Route path="/challengetear" element={<ChallengeTear />} /> */}
+          <Route path="/challengeresult" element={<ChallengeResult />} />
+          <Route path="/challengeImage/:authentication_id" element={<ChallengeImage />} />
+          <Route path="/challengeList" element={<ChallengeList />} />
+          <Route path="/challengeEdit/:challenge_id" element={<ChallengeEdit />} />
+          <Route path="/challengeDetail/:challenge_id" element={<ChallengeDetail />} />
+          <Route path="/challengeDaily/:mission_id" element={<ChallengeDaily />} />
+          <Route path="/alarm" element={<Alarm />} />
+          <Route path="/challengeAccept/:challenge_id" element={<ChallengeAccept />} />
+          <Route path="/fileupload" element={<FileUploadTest />} />
+          <Route path="/friendSelect" element={<FriendSelect />} />
+          <Route path="/challengeResult/:challenge_id" element={<ChallengeResult />} />
+          <Route path="/challengeNotice" element={<ChallengeNotice />} />
+          <Route path="/challengeCreate" element={<ChallengeCreate />} />
+          <Route>
             <Route path="/mypage/" element={<MyPage />} />
-            <Route path="/mypage/:mypageedit" element={<MyPageEdit />} />
-            <Route path="/followlist" element={<FollowList />} />
-
-            {/* 테스트 용 */}
-            <Route path="/test" element={<Test />} />
+            <Route path="/mypage/mypageedit" element={<MyPageEdit />} />
+            <Route path="/mypage/myPagewithdraw" element={<MypageWithdraw />} />
+            <Route path="/mypage/mypagecharge" element={<MypageCharge />} />
+            <Route path="/mypage/followlist" element={<FollowList />} />
+            <Route path="/mypage/mypagehistory" element={<MyPageHistory />} />
+            <Route path="/mypage/mypagehistory/mypagehistorydetail" element={<MyPageHistoryDetail />} />
           </Route>
+
+          {/* 테스트 용 */}
+          <Route path="/test" element={<Test />} />
+          {/* </Route> */}
         </Routes>
       </BrowserRouter>
     </div>
   );
-
 }
 
 export default App;
