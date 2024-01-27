bcb41aa92586b3c57cf0bc990fb2bc626bfe49e5
minsuje <minsu.jex@gmail.com>
2024-01-27 13:23:30 +0900

    feat: create daily service and controller

--- server/src/modules/daily_mission/daily_mission.controller.ts
+++ server/src/modules/daily_mission/daily_mission.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { DailyMissionService } from './daily_mission.service';
 
-@Controller('daily-mission')
-export class DailyMissionController {}
+@Controller('/')
+export class DailyMissionController {
+  constructor(private dailyMissionService: DailyMissionService) {}
+  @Get('/dailyMission')
+  async GetDaily(@Req() req: Request, @Res() res: Response) {
+    return this.dailyMissionService.getDailyMission();
+  }
+}
--- server/src/modules/daily_mission/schema.ts
+++ server/src/modules/daily_mission/schema.ts
@@ -6,7 +6,7 @@ import {
   varchar,
 } from 'drizzle-orm/pg-core';
 
-export const dailyMission = pgTable('dailyMission', {
+export const dailyMission = pgTable('dailymission', {
   mission_id: serial('mission_id').notNull(),
   mission_content: varchar('mission_content', { length: 200 }).notNull(),
   success_userid_num: integer('success_userid_num'),
