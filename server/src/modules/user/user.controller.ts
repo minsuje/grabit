6828a23f3d58e414728cf95fbd9afb18dee44212
yewonkim301 <yeowne301@gmail.com>
2024-01-31 09:12:53 +0900

    feat: get toss payment module

--- server/src/modules/user/user.controller.ts
+++ server/src/modules/user/user.controller.ts
@@ -1,6 +1,19 @@
-import { Body, Controller, Get, Param, Patch, Post, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Redirect,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Response, Request } from 'express';
 import { CreateUserDto, LoginDto } from './dto/create-user.dto';
+import { PaymentDTO } from './dto/paymentsDto';
 import { UserService } from './user.service';
+
 @Controller('/')
 export class UserController {
   constructor(private userService: UserService) {}
@@ -50,4 +63,33 @@ export class UserController {
   getScore(@Param('userid') userid: number) {
     return this.userService.getScore(userid);
   }
+
+  //결제페이지
+  @Get('/checkout/success')
+  success(@Res() res: Response, @Req() req: Request) {
+    console.log('controller success');
+    console.log('req.user >>>> ', req.user);
+    // console.log(res);
+    // res.sendFile('./../../../../client/src/page/CheckoutSuccess.tsx');
+    return res.send('success');
+    // Redirect('/checkout/success');
+  }
+
+  @Post('/checkout')
+  tossPayment(@Body() paymentDTO: PaymentDTO, @Req() req: Request) {
+    console.log('post service url > ', req);
+    console.log('post seevice >> ', paymentDTO);
+    return this.userService.tossPayment(paymentDTO);
+  }
+  // async success(
+  //   @Body() body: PaymentDTO,
+  //   @Res() res: Response,
+  //   @Req() req: Request,
+  // ) {
+  //   console.log('controller body > ', body);
+  //   console.log('controller res > ', res);
+  //   console.log('controller req > ', req);
+  //   return await this.userService.successPay(body, res);
+  //   return;
+  // }
 }
