58c7183a3c0bcce3eef11f41d1da5bdf64c20af8
seejnn <seejinkim@gmail.com>
2024-02-08 13:50:15 +0900

    design: input, cta, label

--- client/src/components/Cta.tsx
+++ client/src/components/Cta.tsx
@@ -3,8 +3,8 @@ import { Button } from './ui/button';
 function Cta({ text, onclick, disabled }: { text: string; onclick: () => void; disabled?: boolean }) {
   return (
     <div className="cta fixed bottom-0 left-0 right-0 flex flex-col">
-      <div className="flex h-8 bg-gradient-to-b from-transparent to-white"></div>
-      <div className="flex bg-white px-8  pb-8 ">
+      <div className="flex h-8 bg-gradient-to-b from-transparent to-grabit-50"></div>
+      <div className="flex bg-grabit-50 px-8 pb-8 ">
         <Button onClick={onclick} className="w-full rounded-md p-6" disabled={disabled}>
           {text}
         </Button>
