0bfcddaa2660dad7320fe85bd73483b13367a0ec
seejnn <seejinkim@gmail.com>
2024-02-08 12:17:24 +0900

    design: withdraw

--- client/src/page/CheckoutSuccess.tsx
+++ client/src/page/CheckoutSuccess.tsx
@@ -1,5 +1,4 @@
-// import axios from '@/api/axios';
-// import axios, { privateApi } from '@/api/axios';
+import axios, { privateApi } from '@/api/axios';
 import Cta from '@/components/Cta';
 import { useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
@@ -8,46 +7,51 @@ export default function CheckoutSuccess() {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const [isConfirmed, setIsConfirmed] = useState(false);
-  // const paymentKey = searchParams.get('paymentKey');
+  const paymentKey = searchParams.get('paymentKey');
   const orderId = searchParams.get('orderId');
   const amount = searchParams.get('amount');
+  // const [error, setError] = useState<boolean>(false);
 
-  // async function confirmPayment() {
-  //   // TODO: API를 호출해서 서버에게 paymentKey, orderId, amount를 넘겨주세요.
-  //   // 서버에선 해당 데이터를 가지고 승인 API를 호출하면 결제가 완료됩니다.
-  //   // https://docs.tosspayments.com/reference#%EA%B2%B0%EC%A0%9C-%EC%8A%B9%EC%9D%B8
-  //   await axios({
-  //     url: '/checkout/confirm',
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     data: JSON.stringify({
-  //       paymentKey,
-  //       orderId,
+  async function confirmPayment() {
+    // TODO: API를 호출해서 서버에게 paymentKey, orderId, amount를 넘겨주세요.
+    // 서버에선 해당 데이터를 가지고 승인 API를 호출하면 결제가 완료됩니다.
+    // https://docs.tosspayments.com/reference#%EA%B2%B0%EC%A0%9C-%EC%8A%B9%EC%9D%B8
+    await axios({
+      url: '/checkout/confirm',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: JSON.stringify({
+        paymentKey,
+        orderId,
 
-  //       amount,
-  //     }),
-  //   }).then((res) => {
-  //     if (res) {
-  //       setIsConfirmed(true);
-  //     }
-  //   });
-  //   await privateApi({
-  //     url: '/updateMoney',
-  //     method: 'POST',
-
-  //     data: {
-  //       amount: amount,
-  //     },
-  //   });
-  // }
+        amount,
+      }),
+    }).then((res) => {
+      if (res) {
+        setIsConfirmed(true);
+      }
+    });
+    await privateApi({
+      url: '/updateMoney',
+      method: 'POST',
+      data: {
+        amount: amount,
+      },
+    }).catch((err) => {
+      // setError(true);
+      console.error(err);
+      alert('캐럿 충전 실패 - 관리자에게 문의해주세요');
+    });
+  }
 
   function handleNavigate() {
     navigate('/mypage');
   }
 
   setTimeout(() => {
+    confirmPayment();
     setIsConfirmed(true);
   }, 2000);
 
@@ -84,7 +88,7 @@ export default function CheckoutSuccess() {
         <div className="confirm-loading flex h-screen w-full flex-col items-center justify-center">
           <div className="flex-column align-center flex flex-col items-center justify-center">
             <img src="https://static.toss.im/lotties/loading-spot-apng.png" width="120" height="120" />
-            <h2 className="title text-center">결제 요청에 성공했습니다.</h2>
+            <h2 className="title text-center">결제중입니다</h2>
           </div>
           {/* <Cta text={'결제 완료'} onclick={confirmPayment} /> */}
         </div>
--- client/src/page/myPage/MyPageWithdraw.tsx
+++ client/src/page/myPage/MyPageWithdraw.tsx
@@ -1,12 +1,15 @@
-import { Button } from '@/components/ui/button';
 import { privateApi } from '@/api/axios';
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setHeaderInfo } from '@/store/headerSlice';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { Input } from '@/components/ui/input';
+import Cta from '@/components/Cta';
+import { useNavigate } from 'react-router-dom';
 
 export default function MyPageWithdraw() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [money, setMoney] = useState<string>('');
   const [bank_num, setBank_num] = useState<string>('');
   const [bank_name, setBank_name] = useState<string>('');
@@ -25,7 +28,10 @@ export default function MyPageWithdraw() {
           bank_name,
           name,
         })
-        .then();
+        .then(() => {
+          alert('출금 신청이 완료되었습니다. 최대 2주일 소요될 수 있습니다.');
+          navigate('/mypage');
+        });
     } catch (error) {
       console.error('Error during API call', error);
     }
@@ -37,35 +43,23 @@ export default function MyPageWithdraw() {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <h1>출금</h1>
-        </div>
-        <div>
-          <input
-            type="text"
+      <div className="flex flex-col gap-8">
+        <h1>출금 신청</h1>
+        <div className="flex flex-col gap-2 ">
+          <h2>출금 금액</h2>
+          <Input
+            type="number"
             name="money"
             placeholder="금액입력"
             value={money}
             onChange={(e) => setMoney(e.target.value)}
           />
-          <p>출금 신청은 10,000원부터 가능합니다.</p>
+          <p className="text-sm text-stone-400">출금 신청은 10,000캐럿부터 가능합니다</p>
         </div>
 
-        <div>
-          <h1>계좌번호</h1>
-          <input
-            type="text"
-            name="bank_num"
-            placeholder="게좌번호를 입력하세요"
-            value={bank_num}
-            onChange={(e) => setBank_num(e.target.value)}
-          />
-        </div>
-
-        <div>
-          <h1>이름</h1>
-          <input
+        <div className="flex flex-col gap-2">
+          <h2>이름</h2>
+          <Input
             type="text"
             name="name"
             placeholder="이름을 입력하세요"
@@ -74,22 +68,30 @@ export default function MyPageWithdraw() {
           />
         </div>
 
-        <Select onValueChange={handleSelectChange}>
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="출금은행" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="카카오">카카오</SelectItem>
-            <SelectItem value="토스">토스</SelectItem>
-            <SelectItem value="국민">국민</SelectItem>
-            <SelectItem value="케이뱅크">케이뱅크</SelectItem>
-            <SelectItem value="농협">농협</SelectItem>
-          </SelectContent>
-        </Select>
-      </form>
-      <Button type="submit" onClick={handleSubmit}>
-        출금 신청
-      </Button>
+        <div className="flex flex-col gap-2">
+          <h2>계좌번호</h2>
+          <Select onValueChange={handleSelectChange}>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="은행을 선택해주세요 " />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="카카오">카카오</SelectItem>
+              <SelectItem value="토스">토스</SelectItem>
+              <SelectItem value="국민">국민</SelectItem>
+              <SelectItem value="케이뱅크">케이뱅크</SelectItem>
+              <SelectItem value="농협">농협</SelectItem>
+            </SelectContent>
+          </Select>
+          <Input
+            type="text"
+            name="bank_num"
+            placeholder="계좌번호를 입력하세요"
+            value={bank_num}
+            onChange={(e) => setBank_num(e.target.value)}
+          />
+        </div>
+        <Cta text={'출금 신청'} onclick={handleSubmit} />
+      </div>
     </div>
   );
 }
