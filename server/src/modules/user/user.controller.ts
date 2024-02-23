06d945a14c8c29343442c017d15b2f561bf791e0
minsuje <minsu.jex@gmail.com>
2024-02-23 17:58:19 +0900

    fix: line 40

--- server/src/modules/user/user.controller.ts
+++ server/src/modules/user/user.controller.ts
@@ -38,7 +38,8 @@ export class UserController {
   // 아이디 중복검사 (register UserID duplicate check)
   @UseGuards(JwtService)
   @Post('/checkid')
-  duplicateCheck(@Body() userid: string) {
+  duplicateCheck(@Body() body: any) {
+    const { userid } = body;
     return this.userService.duplicateCheck(userid);
   }
 
